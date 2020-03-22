2f0a77f919e98cab4ad5e58ca3581f1c9647a273
12408 <12408@DESKTOP-T5JMGGC>
2020-03-22 01:36:33 -0400

    I created VehicleServiceImpl hiskel

---
+++ TrafficAccidentsAnalysisBackEnd/src/main/java/com/trafficaccidentsanalysis/backend/service/serviceimpl/VehicleServiceImpl.java
@@ -0,0 +1,26 @@
+package com.trafficaccidentsanalysis.backend.service.serviceimpl;
+
+import org.hibernate.service.Service;
+import org.hibernate.service.ServiceRegistry;
+
+public class VehicleServiceImpl implements ServiceRegistry {
+
+	@Override
+	public ServiceRegistry getParentServiceRegistry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <R extends Service> R getService(Class<R> serviceRole) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
