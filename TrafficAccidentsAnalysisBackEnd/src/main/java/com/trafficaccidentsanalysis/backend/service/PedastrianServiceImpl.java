55763c4effae74ea5213ec90b82ae1a1c6145490
Seble Misgina <seblem2002@gmail.com>
2020-03-25 13:00:27 -0400

    PedastrianServiceImple----Seble

--- TrafficAccidentsAnalysisBackEnd/src/main/java/com/trafficaccidentsanalysis/backend/service/PedastrianServiceImpl.java
+++ TrafficAccidentsAnalysisBackEnd/src/main/java/com/trafficaccidentsanalysis/backend/service/PedastrianServiceImpl.java
@@ -3,6 +3,7 @@ package com.trafficaccidentsanalysis.backend.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.trafficaccidentsanalysis.backend.model.Pedastrian;
@@ -11,24 +12,45 @@ import com.trafficaccidentsanalysis.backend.repository.PedastrianRepository;
 @Service
 public class PedastrianServiceImpl implements PedastrianService {
 	@Autowired
-	PedastrianRepository pedastianRepository;
+	PedastrianRepository pedastrianRepository;
 
 	@Override
 	public   List<Pedastrian>  getAllPedastrian(){
-		return getAllPedastrian();	
+		return pedastrianRepository.findAll();	
 	}
 	@Override	
 	public   Pedastrian savePedastrian(Pedastrian pedastrian) {
-		return pedastrian;
+		return pedastrianRepository.save(pedastrian) ;
 		
 	}
 	
 	@Override	
-	public void deletePedastrian(Pedastrian pedastrian) {
+	public ResponseEntity<?> deletePedastrian(Pedastrian pedastrian) {
+		return (ResponseEntity<?>) pedastrianRepository.findById(pedastrian.getPedastrianid()).map(pedastrianToDelete->{
+			pedastrianRepository.delete(pedastrianToDelete);
+			return ResponseEntity.ok();
+		}).orElseThrow(null);
 		
 	}
 	@Override
-	 public  void updatePedastrian(Pedastrian pedastrian) {
+	 public  Pedastrian updatePedastrian(Pedastrian pedastrian) {
+		return pedastrianRepository.findById(pedastrian.getPedastrianid()).map(oldPedastrian->{
+			oldPedastrian.setAdditionalInfo(pedastrian.getAdditionalInfo());
+			oldPedastrian.setAddress(pedastrian.getAddress());
+			oldPedastrian.setAge(pedastrian.getAge());
+			oldPedastrian.setFirstName(pedastrian.getFirstName());
+			oldPedastrian.setInjuryClass(pedastrian.getInjuryClass());
+			oldPedastrian.setLastName(pedastrian.getLastName());
+			oldPedastrian.setMiddleName(pedastrian.getMiddleName());
+			oldPedastrian.setPhoneNumber(pedastrian.getPhoneNumber());
+			oldPedastrian.setPositionDuringAccident(pedastrian.getPositionDuringAccident());
+			oldPedastrian.setVehicle(pedastrian.getVehicle());
+			oldPedastrian.setSex(pedastrian.getSex());
+			return pedastrianRepository.save(oldPedastrian);
+		}).orElseThrow(null);
+				
+			}
+		
 		
 	}
-}
+
