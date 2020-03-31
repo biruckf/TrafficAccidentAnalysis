51b9eaf8a456f2ca84506fe9a57b7542cfdecba0
hiskeal <fghiskel1990@gmail.com>
2020-03-30 22:11:13 -0400

    hiskel

--- TrafficAccidentsAnalysisBackEnd/src/main/java/com/trafficaccidentsanalysis/backend/repository/EmployeeRepository.java
+++ TrafficAccidentsAnalysisBackEnd/src/main/java/com/trafficaccidentsanalysis/backend/repository/EmployeeRepository.java
@@ -1,19 +1,11 @@
 package com.trafficaccidentsanalysis.backend.repository;
 
-<<<<<<< HEAD
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.trafficaccidentsanalysis.backend.model.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-=======
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.trafficaccidentsanalysis.backend.model.Employee;
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
->>>>>>> refs/remotes/origin/master
-
+	
 }
